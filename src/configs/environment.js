6228b3752cce46c4a75198c2cfd96ffaa92e57dc
ptt98hd <thanhpham.hd.98@gmail.com>
2025-02-12 06:29:54 +0700

    fixbug: database environment variable

--- src/configs/environment.js
+++ src/configs/environment.js
@@ -4,16 +4,16 @@ dotenv.config();
 
 const variables = {
 	server: {
-		host: process.env.SERVER_HOST,
-		port: process.env.SERVER_PORT,
+		host: process.env.SERVER_HOST || 'localhost',
+		port: process.env.SERVER_PORT || 3000,
 	},
 	database: {
-		dialect: process.env.DB_DIALECT,
-		host: process.env.DB_HOST,
-		port: process.env.DB_PORT,
-		name: process.env.DB_NAME,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
+		dialect: process.env.DB_DIALECT || 'mysql',
+		host: process.env.DB_HOST || 'localhost',
+		port: process.env.DB_PORT || 3306,
+		name: process.env.DB_NAME || 'village_market',
+		user: process.env.DB_USER || 'root',
+		password: process.env.DB_PASSWORD || '12345678',
 	},
 };
 
